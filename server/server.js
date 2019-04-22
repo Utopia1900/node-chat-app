2fbd855af75f5d757491d5299864b5e46289b5a2
Utopia1900 <xiongzecong@gmail.com>
2019-04-22 17:31:44 +0800

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -9,32 +9,21 @@ const port = process.env.PORT || 3000
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var {generateMessage} = require('./utils/message');
 
 app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
     socket.on('createMessage', (message) => {
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+        io.emit('newMessage', generateMessage(message.from, message.text));
         // socket.broadcast.emit('newMessage', {
         //   from: message.from,
         //   text: message.text,
         //   createdAt: new Date().getTime()
         // })
     })
-    socket.emit('newMessage', {
-        from: 'admin',
-        text: 'welcome to the chat app',
-        createdAt: new Date().getTime()
-    })
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-      })
+    socket.emit('newMessage',generateMessage('Admin', 'welcome to the chat app'));
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     })
---
+++ server/utils/message.test.js
@@ -0,0 +1,12 @@
+var {generateMessage} = require('./message');
+var expect = require('expect');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'Jen';
+        var text = 'Some message';
+        var message = generateMessage(from, text);
+        expect(message.createdAt).not.toBeNaN();
+        expect(message).toMatchObject({from, text});
+    })
+})
